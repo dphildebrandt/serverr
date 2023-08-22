517d24b6cec82d3710fed0778d88b62857b40792
Dustin Hildebrandt <dustin.hildebrandt@gmail.com>
2023-08-22 08:42:47 -0500

    Use CrowdSec and traefik-bouncer

--- backup.sh
+++ backup.sh
@@ -7,6 +7,6 @@ containers=`docker ps -f "label=backup.stop" --format {{.Names}} | sort`
 docker stop $containers
 
 rclone sync $DOCKER_DIR/app-data Dropbox:serverr-appdata \
-    -L -P --log-level=NOTICE --log-file=$LOG_DIR/appdata.log
+    -L -P --log-level=NOTICE --log-file=$DOCKER_DIR/logs/appdata.log
 
 docker start $containers
