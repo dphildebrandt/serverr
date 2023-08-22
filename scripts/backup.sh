4efcaeb80432d21b4fe6b1459e709e79e02a7fee
Dustin Hildebrandt <dustin.hildebrandt@gmail.com>
2023-08-22 10:17:58 -0500

    Update backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,6 +7,6 @@ containers=`docker ps -f "label=backup.stop" --format {{.Names}} | sort`
 docker stop $containers
 
 rclone sync $DOCKER_DIR/app-data Dropbox:serverr-appdata \
-    -L -P --log-level=NOTICE --log-file=$DOCKER_DIR/logs/appdata.log
+    -P --skip-links --log-level=NOTICE --log-file=$DOCKER_DIR/logs/appdata.log
 
 docker start $containers
