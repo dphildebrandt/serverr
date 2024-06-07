7303507a15daa00ad4f8003dad0d7bcdc1a1afc6
Dustin Hildebrandt <dustin.hildebrandt@gmail.com>
2024-06-07 16:25:33 -0500

    Improve backup script speed

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,6 +7,7 @@ containers=`docker ps -f "label=backup.stop" --format {{.Names}} | sort`
 docker stop $containers
 
 rclone sync $DOCKER_DIR/app-data Dropbox:serverr-appdata \
-    -P --skip-links --log-level=NOTICE --log-file=$DOCKER_DIR/logs/appdata.log
+    -P --skip-links --log-level=NOTICE --log-file=$DOCKER_DIR/logs/appdata.log \
+    --tpslimit 5 --tpslimit-burst 0
 
 docker start $containers
